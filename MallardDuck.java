6e33018001715ff762e1d9c92aed4683d9098bf0
unknown <vidish162005@gmail.com>
2025-03-08 19:45:40 +0530

    added MallardDuck.java

---
+++ MallardDuck.java
@@ -0,0 +1,13 @@
+public class MallardDuck extends Duck{
+
+    public MallardDuck(){
+        quackBehaviour=new Quack();
+        swimBehaviour= new SwimWithLegs();
+        flyBehaviour = new Fly();
+
+    }
+    @Override
+    public void display() {
+        System.out.println("I am Mallard Duck !");
+    }
+}
